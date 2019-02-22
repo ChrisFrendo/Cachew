8404e5eebe225e2495389c3e572475635cf05aaa
ChrisFrendo <chris.frendo.18@um.edu.mt>
2019-02-22 14:30:28 +0100

    Created Schemas for question and study

--- rest_api/models/question.js
+++ rest_api/models/question.js
@@ -3,7 +3,19 @@ const Schema = mongoose.Schema;
 
 // create question Schema & model
 const QuestionSchema = new Schema({
-
+  title: {
+    type: String,
+    required: true
+  },
+  question_type: {
+    type: String,
+    enum: ["scale", "freetext", "bool", "mult"],
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+  }
 });
 
 const Question = mongoose.model('question', QuestionSchema);
--- rest_api/models/study.js
+++ rest_api/models/study.js
@@ -5,7 +5,21 @@ const Question = require('../models/question');
 
 // create study Schema & model
 const StudySchema = new Schema({
-
+  title: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Date,
+    default: Date.now(),
+  },
+  userId: {
+    type: Schema.ObjectId,
+    required: true
+  },
+  questions: [{
+    type: Schema.ObjectId,
+  }]
 });
 
 const Study = mongoose.model('study', StudySchema);
