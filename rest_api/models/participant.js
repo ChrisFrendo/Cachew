ba82d795d77c240551655fb3a72fb1e695998e71
Daniel Sumler <d.sumler1@outlook.com>
2019-02-28 12:34:18 +0100

    Adding the participants data type

--- rest_api/models/participant.js
+++ rest_api/models/participant.js
@@ -3,7 +3,27 @@ const Schema = mongoose.Schema;
 
 // create participant Schema & model
 const ParticipantSchema = new Schema({
-  
+  username:{
+  type:String,
+  required:[true, 'Username field is required'],
+  unique:true
+},
+password:{
+  type:String,
+  required:[true, 'Password field is required']
+},
+dob:{
+  type:Date,
+  required:[true, 'Date of Birth is required to sign up']
+},
+salary:{
+  type:Number,
+  required:[true, 'Salary field is required']
+},
+industry:{
+  type:String,
+  required:[true, 'Industry field is required']
+}
 });
 
 const Participant = mongoose.model('participant', ParticipantSchema);
