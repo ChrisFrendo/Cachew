14f08c41dea10a16ff50605893436e3e510e814f
Esther <esther.spiteri.18@um.edu.mt>
2019-03-07 22:04:48 +0100

    Set Up home page, basic layout of log-in page and generated sign-up page.

--- surveySays/src/app/app-routing.module.ts
+++ surveySays/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
+  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
+  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
+  { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
 ];
 
 @NgModule({
---
+++ surveySays/src/app/log-in/log-in.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.page.html',
+  styleUrls: ['./log-in.page.scss'],
+})
+export class LogInPage {
+
+  constructor(private router: Router) {}
+
+  goToSignUp(){
+    this.router.navigateByUrl('/sign-up');
+  }
+
+}
