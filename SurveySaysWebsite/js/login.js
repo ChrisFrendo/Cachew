8153cb7aa6d6409feaf97fd4225c21e6a504074d
ChrisFrendo <chris.frendo.18@um.edu.mt>
2019-03-11 21:48:42 +0100

    Fixed login (still not using token)

--- SurveySaysWebsite/js/login.js
+++ SurveySaysWebsite/js/login.js
@@ -3,26 +3,26 @@ function login(){
   var username = document.getElementById('loginUsername').value;
   var password = document.getElementById('loginPassword').value;
 
-  var url = "http://localhost:4000/api/login?username=" + username + "&password=" + password;
+  var url = "http://localhost:4000/api/login/researcher?username=" + username + "&password=" + password;
 
   // console.log(url);
 
   $.ajax({
-    success: function(data){
-      console.log(data);
-        if (data.found === true){
+    contentType: 'application/json',
+    success: function(data, textStatus, xhr){
+        if (xhr.status == 200){
           window.alert("Account found. Logging in now...");
-          window.location = "index.html";
+          window.location.assign("index.html");
           return false;
         } else {
           window.alert("Account not found, please check username and password");
         }
     },
     error: function(jqXHR, exception){
-      console.log("Something went wrong when processing the request");
-      alert("Something went wrong when processing the request");
+      window.alert("Account not found, please check username and password");
     },
     type: 'GET',
-    url: url
+    url: url,
+    async: true
     });
 }
