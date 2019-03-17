d833f1fd2a248ed03f58bb112ba7ab60127a7b4f
ChrisFrendo <chris.frendo.18@um.edu.mt>
2019-03-17 12:03:14 +0100

    Added functionality that stores token in local storage upon successful login

--- SurveySaysWebsite/js/login.js
+++ SurveySaysWebsite/js/login.js
@@ -3,15 +3,16 @@ function login(){
   var username = document.getElementById('loginUsername').value;
   var password = document.getElementById('loginPassword').value;
 
-  var url = "http://localhost:4000/api/login/researcher?username=" + username + "&password=" + password;
+  var ip = '192.168.1.83';
 
-  // console.log(url);
+  var url = "http://"+ip+":4000/api/login/researcher?username=" + username + "&password=" + password;
 
   $.ajax({
     contentType: 'application/json',
     success: function(data, textStatus, xhr){
         if (xhr.status == 200){
           window.alert("Account found. Logging in now...");
+          localStorage.setItem('token', JSON.stringify(data.token));
           window.location.assign("index.html");
           return false;
         } else {
