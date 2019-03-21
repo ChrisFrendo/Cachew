dec09a2da94bdf7bffdfb4e71971ea5593882d53
tristan-oa <tristanoagalea@gmail.com>
2019-03-21 12:51:37 +0100

    Added scaleOption() function and fixed problem with delete button

--- SurveySaysWebsite/js/create-study.js
+++ SurveySaysWebsite/js/create-study.js
@@ -4,7 +4,7 @@ var count= 0;
 $(document).ready(function() {
   $("#btnNewQuestion").click(function(e) {
     e.preventDefault();
-    $("#testForm").append("<div id='questionDiv'><div class='form-group'><input type='text' name='questionTitle' required='true' placeholder='Question Title' class='form-control form-control-user'/></div><div class='form-group'><select id='selectForm' class='form-control form-control-user' name='questionType'></select></div><div class='form-group'><textarea required='true' rows='10' class='form-control form-control-user' type='text' name='questionContent' placeholder='Question Content'></textarea></div><div class='form-group'><button class='btn btn-primary btn-user form-control' type='button' onclick='removeQuestion(this)'>Delete Question</button></div></div>");
+    $("#testForm").append("<div id='questionDiv'><div class='form-group'><input type='text' name='questionTitle' required='true' placeholder='Question Title' class='form-control form-control-user'/></div><div class='form-group'><select id='selectForm' class='form-control form-control-user' name='questionType'></select></div><div class='form-group'><textarea required='true' rows='10' class='form-control form-control-user' type='text' name='questionContent' placeholder='Question Content'></textarea></div><div class='form-group' id='paramaters'></div><div id='delete-button' class='form-group'><button class='btn btn-primary btn-user form-control' type='button' onclick='removeQuestion(this)'>Delete Question</button></div></div>");
 
     var questionTypeSelect = document.getElementsByName("questionType");
 
@@ -20,8 +20,83 @@ $(document).ready(function() {
     }
     count++;
 
+    $("select").on("change", function(){
+     var option = $(this).val();
+     $(".field").hide();
+     switch (option) {
+       case "Multiple Choice":
+         multipleChoice(this);
+         break;
+       case "Scale":
+         scaleOption(this);
+         break;
+        case "Free Text":
+        var paramaters = this.parentNode.nextSibling.nextSibling;
+        paramaters.innerHTML = "";
+         break;
+       case "Boolean":
+        booleanOption(this);
+        break;
+     }
+});
+
 });
 });
+
+function scaleOption(element)
+{
+  var paramaters = element.parentNode.nextSibling.nextSibling;
+
+  paramaters.innerHTML = "";
+
+  var min = document.createElement('input');
+  min.setAttribute("placeholder", "Min value");
+  min.setAttribute("name", "input1");
+  min.setAttribute("class", "form-group form-control form-control-user");
+
+  var max = document.createElement('input');
+  max.setAttribute("placeholder", "Max Value");
+  max.setAttribute("name", "input2");
+  max.setAttribute("class", "form-group form-control form-control-user");
+
+  paramaters.appendChild(min);
+  paramaters.appendChild(max);
+
+}
+
+function multipleChoice(element)
+{
+  var paramaters = element.parentNode.nextSibling.nextSibling;
+
+  paramaters.innerHTML = "";
+}
+
+function booleanOption(element)
+{
+  var paramaters = element.parentNode.nextSibling.nextSibling;
+
+  paramaters.innerHTML = "";
+
+  var truevalue = document.createElement('input');
+  truevalue.setAttribute("placeholder", "True Value");
+  truevalue.setAttribute("name", "input1");
+  truevalue.setAttribute("class", "form-group form-control form-control-user");
+
+  var falsevalue = document.createElement('input');
+  falsevalue.setAttribute("placeholder", "False Value");
+  falsevalue.setAttribute("name", "input2");
+  falsevalue.setAttribute("class", "form-group form-control form-control-user");
+
+  paramaters.appendChild(truevalue);
+  paramaters.appendChild(falsevalue);
+
+
+
+
+
+
+
+}
 
 
 var questionTypes = [];
@@ -90,12 +165,19 @@ function submitStudy(){
   var questionTypes = document.getElementsByName('questionType');
   var questionContents = document.getElementsByName('questionContent');
 
+  var input1 = document.getElementsByName('input1');
+  var input2 = document.getElementsByName('input2');
+
+
   var i;
   for (i = 0; i < count; i++){
     var question = new Object();
     question.title = questionTitles[i].value;
     question.type = questionTypes[i].value;
     question.content = questionContents[i].value;
+
+    console.log(input1[i].value);
+    console.log(input2[i].value);
 
     var questionJsonString = JSON.stringify(question);
 
