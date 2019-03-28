525662717b4d3ed15d3ff738d5c3800abee72405
Esther <esther.spiteri.18@um.edu.mt>
2019-03-28 10:59:10 +0100

    Added badges to notify number of questions available
    Co-Authored-By: null <francescac5@users.noreply.github.com>

--- surveySays/src/app/study/study.page.ts
+++ surveySays/src/app/study/study.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Http} from '@angular/http';
+import { ToastController } from '@ionic/angular';
+import { async } from 'q';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-study',
@@ -6,10 +12,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./study.page.scss'],
 })
 export class StudyPage implements OnInit {
+  ip: string = '10.60.10.66';
 
-  constructor() { }
+  genreSelect: string[] = [];
+  studyTitles: Array<any>;
+  studyId: Array<any>;
 
-  ngOnInit() {
+  constructor(private menu: MenuController,private storage: Storage, private router: Router, private http: Http, private toastController: ToastController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
   }
 
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  subscribed(){
+    this.router.navigateByUrl('/dashboard');
+  }
+
+  new(){
+    this.router.navigateByUrl('/new');
+  }
+
+  ngOnInit() {
+    //get selected subscribed to study
+    // this.input="";
+    // this.genreSelect[0]="all";
+    // console.log(this.genreSelect);
+    // this.storage.get('token').then((val) => {
+    //   console.log('Your token is', val);
+    //   this.http.get('http://'+this.ip+':4000/api/study/notsubscribed?token=' + val+"&title=" + this.input+"&genres=" + this.genreSelect).subscribe(data => {
+    //     this.studyTitles = JSON.parse((<any>data)._body).array;
+    //     this.studyId = JSON.parse((<any>data)._body).array;
+    //     for(var i=0; i<this.studyTitles.length; i++){
+    //       this.studyTitles[i] = this.studyTitles[i].title;
+    //       this.studyId[i] = this.studyId[i]._id;
+    //     }
+    //   }, error => {
+    //     this.presentToast("Error when retrieving data. Please try again later");
+    //     console.log(error);
+    //   })
+    // })
+  }
+
+  async presentToast(displayMessage) {
+    const toast = await this.toastController.create({
+      message: displayMessage,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
