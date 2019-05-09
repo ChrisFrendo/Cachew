9fe28d47fad39f4cb109f02fe7091e982d3582a0
ChrisFrendo <chris.frendo.18@um.edu.mt>
2019-05-09 11:10:22 +0200

    Added live updates to reports

--- SurveySaysWebsite/js/reports.js
+++ SurveySaysWebsite/js/reports.js
@@ -12,6 +12,7 @@ var getReportsURL = "http://"+ip+":4000/api/report?token="+token;
 var dataLabels = [];
 var dataValues = [];
 var questionNames = [];
+var chartTypes = [];
 var counter = 0;
 
 
@@ -92,6 +93,8 @@ $("select").change(function() {
     if(studies[j].title == newSelection) {
       console.log(studies[j].questions);
       for(var k=0; k < studies[j].questions.length; k++){
+          chartTypes[k] = 'line';
+
         if (dataLabels[k].length == 0 && dataValues[k].length != 0){
           $("#chartCanvas").append("<div class='card shadow mb-4' name='reportChart'><div class='card-header py-3'><h6 class='m-0 font-weight-bold text-primary'>"+questionNames[k]+"</h6></div><textarea class='card-body' readonly rows="+dataValues[k].length+">"+dataValues[k]+"</textarea><label name='counter' hidden='true'>"+k+"</label></div>");
 
@@ -100,22 +103,78 @@ $("select").change(function() {
 
         } else {
         $("#chartCanvas").append("<div class='card shadow mb-4' name='reportChart'><div class='card-header py-3'><h6 class='m-0 font-weight-bold text-primary'>"+questionNames[k]+"</h6></div><div class='card-body'><div class='chart-bar'><canvas name='SampleAnswerReport'></canvas></div><hr><div class='icon-bar text-right'><label name='counter' hidden='true'>"+k+"</label><label><input type='button' onclick='updateChart(this)' name='chartType' value='bar' hidden='true'><i class='fa fa-chart-bar fa-lg' style='color:royalblue'></i></a> </label><label><input type='button' hidden='true' onclick='updateChart(this)' name='chartType' value='doughnut'><i class='fa fa-chart-pie fa-lg' style='color:royalblue'></i></label> <label><input hidden='true' type='button'  onclick='updateChart(this)' name='chartType' value='line'><i class='fa fa-chart-line fa-lg' style='color:royalblue'></i></a></label> <label><input hidden='true' type='button' onclick='updateChart(this)' name='chartType' value='radar'><img aria-hidden='true' class='fa fa-chart-area fa-lg' src='img/radar-plot-24.ico' alt='img'></i></label></div></div></div>");
-        createChart(k);
+        createChart(k, chartTypes[k]);
       }
       }
     }
-
   }
 })
+
+setInterval(function () {
+  var newSelection = $.trim( $(".form-control option:selected").text());
+  // console.log(selectedItem);
+  console.log(newSelection);
+  //debugger;
+
+  for(var i =0; i < studies.length; i++)
+  {
+    if(newSelection == studies[i].title)
+    {
+      studyID = studies[i]._id;
+      console.log(studyID);
+    }
+  }
+  var getReportsDataURL = "http://"+ip+":4000/api/getData?token="+token+"&studyID="+studyID;
+
+
+  $.ajax({
+    contentType: 'application/json',
+    type: 'GET',
+    url: getReportsDataURL,
+    async: false,
+    success: function(data){
+      // debugger;
+      dataValues = data.values;
+      dataLabels = data.labels;
+      questionNames = data.titles;
+    },
+    error: function(jqXHR, exception){
+      console.log("Something went wrong");
+    }
+  });
+
+  var chartCanvas = document.getElementById('chartCanvas');
+
+  counter = chartCanvas.childElementCount;
+  for (var i = 0; i < counter; i++){
+    chartCanvas.removeChild(chartCanvas.firstChild);
+  }
+
+  for(var j = 0; j < studies.length; j ++) {
+    if(studies[j].title == newSelection) {
+      console.log(studies[j].questions);
+      for(var k=0; k < studies[j].questions.length; k++){
+        if (dataLabels[k].length == 0 && dataValues[k].length != 0){
+          $("#chartCanvas").append("<div class='card shadow mb-4' name='reportChart'><div class='card-header py-3'><h6 class='m-0 font-weight-bold text-primary'>"+questionNames[k]+"</h6></div><textarea class='card-body' readonly rows="+dataValues[k].length+">"+dataValues[k]+"</textarea><label name='counter' hidden='true'>"+k+"</label></div>");
+
+        } else if (dataLabels[k].length == 0 && dataValues[k].length == 0){
+          $("#chartCanvas").append("<div class='card shadow mb-4'><div class='card-header py-3'><h6 class='m-0 font-weight-bold text-primary'>"+questionNames[k]+"</h6></div><textarea class='card-body' readonly rows="+dataValues[k].length+">No answers available</textarea><label name='counter' hidden='true'>"+k+"</label></div>");
+
+        } else {
+        $("#chartCanvas").append("<div class='card shadow mb-4' name='reportChart'><div class='card-header py-3'><h6 class='m-0 font-weight-bold text-primary'>"+questionNames[k]+"</h6></div><div class='card-body'><div class='chart-bar'><canvas name='SampleAnswerReport'></canvas></div><hr><div class='icon-bar text-right'><label name='counter' hidden='true'>"+k+"</label><label><input type='button' onclick='updateChart(this)' name='chartType' value='bar' hidden='true'><i class='fa fa-chart-bar fa-lg' style='color:royalblue'></i></a> </label><label><input type='button' hidden='true' onclick='updateChart(this)' name='chartType' value='doughnut'><i class='fa fa-chart-pie fa-lg' style='color:royalblue'></i></label> <label><input hidden='true' type='button'  onclick='updateChart(this)' name='chartType' value='line'><i class='fa fa-chart-line fa-lg' style='color:royalblue'></i></a></label> <label><input hidden='true' type='button' onclick='updateChart(this)' name='chartType' value='radar'><img aria-hidden='true' class='fa fa-chart-area fa-lg' src='img/radar-plot-24.ico' alt='img'></i></label></div></div></div>");
+        createChart(k, chartTypes[k]);
+      }
+      }
+    }
+  }
+}, 30 * 1000);
 
 //let userData = dataValues;
 var coloursArray = ['#4e73df', '#1cc88a', '#FF8800', '#36b9cc', '#f50057', '#ffcc00', '#ff66ff', '#69f0ae', '#ffab40']
 
 var data;
 
-function createChart(counter){
-
-
+function createChart(counter, type){
   data = {
 
     labels: dataLabels[counter],
@@ -137,8 +196,26 @@ function createChart(counter){
     // debugger;
     var ctx = sampleAnswers[counter].getContext('2d');
     var sampleChart = new Chart(ctx, {
-      type: 'line',
-      data: data
+      type: type,
+      data: data,
+      gridLines: {
+        display: false,
+        drawBorder: false
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            // Change here
+            barPercentage: 0.4
+          }]
+        }
+
+      },
     });
 
   }
@@ -152,6 +229,8 @@ function createChart(counter){
     chartCard.innerHTML = "<div class='card-header py-3'><h6 class='m-0 font-weight-bold text-primary'>"+questionNames[questionNumber]+"</h6></div><div class='card-body'><div class='chart-bar'><canvas name='SampleAnswerReport'></canvas></div><hr><div class='icon-bar text-right'><label name='counter' hidden='true'>"+questionNumber+"</label> <label><input hidden='true' type='button' onclick='updateChart(this)' name='chartType' value='bar'><i class='fa fa-chart-bar fa-lg' style='color:royalblue'></i></a> </label><label><input hidden='true' type='button' onclick='updateChart(this)' name='chartType' value='doughnut'><i class='fa fa-chart-pie fa-lg' style='color:royalblue'></i></label> <label><input hidden='true' type='button'  onclick='updateChart(this)' name='chartType' value='line'><i class='fa fa-chart-line fa-lg' style='color:royalblue'></i></a></label> <label><input hidden='true' type='button' onclick='updateChart(this)' name='chartType' value='radar'><img aria-hidden='true' class='fa fa-chart-area fa-lg' src='img/radar-plot-24.ico' alt='img'></i></label></div></div>"
 
     var myCTX = chartCard.firstChild.nextSibling.firstChild.firstChild;
+
+    chartTypes[questionNumber] = chartType;
 
     data = {
 
